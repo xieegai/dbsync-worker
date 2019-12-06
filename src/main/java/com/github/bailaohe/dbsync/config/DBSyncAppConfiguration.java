81f57aceee5d6fcd864680f5525788bd7ab0a20b
bailaohe <bailaohe@gmail.com>
2019-12-06 22:31:34 +0800

    updated

--- src/main/java/com/github/bailaohe/dbsync/config/DBSyncAppConfiguration.java
+++ src/main/java/com/github/bailaohe/dbsync/config/DBSyncAppConfiguration.java
@@ -1,14 +1,31 @@
 package com.github.bailaohe.dbsync.config;
 
+import com.github.bailaohe.dbsync.publish.DBSyncPublisher;
 import com.github.bailaohe.dbsync.subscribe.DBSyncListener;
 import com.github.bailaohe.dbsync.subscribe.DBSyncProcessor;
+import com.github.bailaohe.repository.sync.IDBSyncPublisher;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class DBSyncAppConfiguration {
     @Bean
     public DBSyncListener dbSyncListener(DBSyncProcessor dbSyncProcessor) {
         return new DBSyncListener(dbSyncProcessor);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public IDBSyncPublisher dbSyncPublisher() {
+        DBSyncPublisher publisher = new DBSyncPublisher();
+        publisher.setExecutor(new ThreadPoolExecutor(8, 32,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>()));
+        return publisher;
     }
 }
